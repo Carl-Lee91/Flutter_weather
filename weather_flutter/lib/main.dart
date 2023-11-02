edb0ed437d5da64c4a270ce34459ad29246aacf3
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-11-02 12:17:59 +0900

    기능 추가: API 닷엔브 추가

--- weather_flutter/lib/main.dart
+++ weather_flutter/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_flutter/constant/sizes.dart';
 import 'package:weather_flutter/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MyApp()));
 }
 
