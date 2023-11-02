8b1bed37665761e34af0b658064ccb623f2940bd
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-11-02 12:20:20 +0900

    스크린 리팩터링: 주간 날씨
    러프 API 페치 결과 추가 및 러프 오늘의 날씨 추가

--- weather_flutter/lib/screens/weekWeather/week_weather.dart
+++ weather_flutter/lib/screens/weekWeather/week_weather.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_flutter/api/api_services.dart';
+import 'package:weather_flutter/api/model/weather_api_model.dart';
+import 'package:weather_flutter/constant/sizes.dart';
+import 'package:weather_flutter/screens/widgets/today_weather.dart';
 
 class WeekWeather extends ConsumerStatefulWidget {
   const WeekWeather({super.key});
@@ -9,8 +13,43 @@ class WeekWeather extends ConsumerStatefulWidget {
 }
 
 class _WeekWeatherState extends ConsumerState<WeekWeather> {
+  late Future<WeatherModel> weather;
+
+  double lat = 37.67;
+  double lon = 126.75;
+
+  @override
+  void initState() {
+    super.initState();
+    weather = ApiService.fetchWeatherInfo(lat, lon);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size28,
+          vertical: Sizes.size28,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TodaysWeather(),
+            FutureBuilder(
+              future: weather,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [Text(snapshot.data!.cityName)],
+                  );
+                }
+                return const Text("...");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
