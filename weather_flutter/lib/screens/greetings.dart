8a14cfd3719fe88625c96d30263fed554cf8ea73
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-11-03 16:07:29 +0900

    기능추가 + 스크린 리팩터링: 위치 정보 확인
    위치 정보 확인과 거절시 확인하는 기능을 만듬

--- weather_flutter/lib/screens/greetings.dart
+++ weather_flutter/lib/screens/greetings.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:weather_flutter/constant/sizes.dart';
 import 'package:weather_flutter/screens/widgets/button.dart';
 
-class GreetingsScreen extends StatelessWidget {
+class GreetingsScreen extends StatefulWidget {
   static const routeName = "greetings";
   static const routeUrl = "/";
   const GreetingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    void _onEnterAppTab() {
-      context.go("/todayWeather");
-    }
+  State<GreetingsScreen> createState() => _GreetingsScreenState();
+}
 
+class _GreetingsScreenState extends State<GreetingsScreen> {
+  Future<void> _requestLocationPermissionAndEnterApp() async {
+    PermissionStatus status = await Permission.location.status;
+    status = await Permission.location.request();
+    if (!mounted) return;
+    if (status.isGranted) {
+      context.go("/todayWeather");
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text(
+              "위치 정보 권한 확인",
+            ),
+            content: const Text(
+              "위치 정보 권한을 허가하여 주십시오.",
+            ),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  status = await Permission.location.request();
+                  if (!mounted) return;
+                  if (status.isGranted) {
+                    context.go("/todayWeather");
+                  }
+                },
+                child: const Text(
+                  "허가",
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -30,7 +69,7 @@ class GreetingsScreen extends StatelessWidget {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(vertical: Sizes.size16),
         child: GestureDetector(
-          onTap: _onEnterAppTab,
+          onTap: _requestLocationPermissionAndEnterApp,
           child: const Button(text: "날씨 알아보기"),
         ),
       ),
