b37ba58ec3b82581b08683cc0904212be5e92946
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-11-07 22:08:37 +0900

    리팩터링: Container추가

--- weather_flutter/lib/screens/widgets/basic_weather/view/basic_weather.dart
+++ weather_flutter/lib/screens/widgets/basic_weather/view/basic_weather.dart
@@ -52,75 +52,85 @@ class _BasicWeatherState extends ConsumerState<BasicWeather> {
         } else if (snapshot.hasError) {
           return Text('에러발생: ${snapshot.error}');
         } else if (snapshot.hasData) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "${now.month}월 ${now.day}일 ${now.hour} : ${now.minute.toString().padLeft(2, '0')}",
-                        style: const TextStyle(fontSize: Sizes.size16),
-                      ),
-                      IconButton(
-                        onPressed: _refreshTimeAndWeather,
-                        icon: const FaIcon(
-                          FontAwesomeIcons.arrowsRotate,
-                          size: Sizes.size16,
+          return Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size10,
+              vertical: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor.withOpacity(0.05),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "${now.month}월 ${now.day}일 ${now.hour} : ${now.minute.toString().padLeft(2, '0')}",
+                          style: const TextStyle(fontSize: Sizes.size16),
                         ),
-                      )
-                    ],
-                  ),
-                  Gaps.v10,
-                  Row(
-                    children: [
-                      Text(
-                        "${snapshot.data!.temp.toStringAsFixed(1).toString()} ℃",
-                        style: const TextStyle(fontSize: Sizes.size32),
-                      ),
-                    ],
-                  ),
-                  Gaps.v10,
-                  Text(
-                    snapshot.data!.cityName,
-                    style: const TextStyle(fontSize: Sizes.size20),
-                  ),
-                  Gaps.v10,
-                  Text(
-                    "${snapshot.data!.maxTemp.toStringAsFixed(1).toString()} ℃ / ${snapshot.data!.minTemp.toStringAsFixed(1).toString()} ℃",
-                    style: const TextStyle(fontSize: Sizes.size16),
-                  ),
-                  Text(
-                    "체감온도 ${snapshot.data!.feelTemp.toStringAsFixed(1).toString()} ℃",
-                    style: const TextStyle(fontSize: Sizes.size16),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.network(
-                    "https://openweathermap.org/img/wn/${snapshot.data!.weather.map((weather) => weather.icon).join(", ")}@2x.png",
-                    scale: 1.5,
-                  ),
-                  Text(
-                    snapshot.data!.weather
-                        .map((weather) => weather.description)
-                        .join(", "),
-                    style: const TextStyle(fontSize: Sizes.size20),
-                  ),
-                  Gaps.v10,
-                  Text(
-                    "습도 ${snapshot.data!.humidity.toStringAsFixed(1).toString()} %",
-                    style: const TextStyle(fontSize: Sizes.size16),
-                  ),
-                  Text(
-                    "풍속 ${snapshot.data!.windSpeed.toStringAsFixed(1).toString()} m/s",
-                    style: const TextStyle(fontSize: Sizes.size16),
-                  ),
-                ],
-              ),
-            ],
+                        IconButton(
+                          onPressed: _refreshTimeAndWeather,
+                          icon: const FaIcon(
+                            FontAwesomeIcons.arrowsRotate,
+                            size: Sizes.size16,
+                          ),
+                        )
+                      ],
+                    ),
+                    Gaps.v10,
+                    Row(
+                      children: [
+                        Text(
+                          "${snapshot.data!.temp.toStringAsFixed(1).toString()} ℃",
+                          style: const TextStyle(fontSize: Sizes.size32),
+                        ),
+                      ],
+                    ),
+                    Gaps.v10,
+                    Text(
+                      snapshot.data!.cityName,
+                      style: const TextStyle(fontSize: Sizes.size20),
+                    ),
+                    Gaps.v10,
+                    Text(
+                      "${snapshot.data!.maxTemp.toStringAsFixed(1).toString()} ℃ / ${snapshot.data!.minTemp.toStringAsFixed(1).toString()} ℃",
+                      style: const TextStyle(fontSize: Sizes.size16),
+                    ),
+                    Text(
+                      "체감온도 ${snapshot.data!.feelTemp.toStringAsFixed(1).toString()} ℃",
+                      style: const TextStyle(fontSize: Sizes.size16),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.network(
+                      "https://openweathermap.org/img/wn/${snapshot.data!.weather.map((weather) => weather.icon).join(", ")}@2x.png",
+                      scale: 1.2,
+                    ),
+                    Text(
+                      snapshot.data!.weather
+                          .map((weather) => weather.description)
+                          .join(", "),
+                      style: const TextStyle(fontSize: Sizes.size20),
+                    ),
+                    Gaps.v10,
+                    Text(
+                      "습도 ${snapshot.data!.humidity.toStringAsFixed(1).toString()} %",
+                      style: const TextStyle(fontSize: Sizes.size16),
+                    ),
+                    Text(
+                      "풍속 ${snapshot.data!.windSpeed.toStringAsFixed(1).toString()} m/s",
+                      style: const TextStyle(fontSize: Sizes.size16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         }
         return const Center(
