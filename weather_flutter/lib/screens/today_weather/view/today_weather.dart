5e94edfb44a72a9e8d3cad4024a7a17d2b78dde3
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-11-09 08:49:58 +0900

    리팩터링: 미세먼지 색, 텍스트
    미세먼지 농도에 따라 색과 텍스트가 변화하는 것 추가

--- weather_flutter/lib/screens/today_weather/view/today_weather.dart
+++ weather_flutter/lib/screens/today_weather/view/today_weather.dart
@@ -95,6 +95,50 @@ class _TodayWeatherState extends ConsumerState<TodayWeather> {
                 if (snapshot.hasError) {
                   return Text('에러발생: ${snapshot.error}');
                 } else if (snapshot.hasData) {
+                  final pm2_5Pollution = snapshot.data!.airPollution
+                      .map((airPollution) => airPollution.pm2_5)
+                      .join(", ");
+                  final pm10Pollution = snapshot.data!.airPollution
+                      .map((airPollution) => airPollution.pm10)
+                      .join(", ");
+                  num pm2_5Num = num.parse(pm2_5Pollution);
+                  num pm10Num = num.parse(pm10Pollution);
+                  Color pm2_5Color = Colors.white;
+                  Color pm10Color = Colors.white;
+                  String pm2_5Text = "";
+                  String pm10Text = "";
+                  if (pm2_5Num <= 10) {
+                    pm2_5Color = Colors.blue;
+                    pm2_5Text = "최상";
+                  } else if (pm2_5Num > 10 && pm2_5Num <= 25) {
+                    pm2_5Color = Colors.green;
+                    pm2_5Text = "좋음";
+                  } else if (pm2_5Num > 25 && pm2_5Num <= 50) {
+                    pm2_5Color = Colors.yellow;
+                    pm2_5Text = "보통";
+                  } else if (pm2_5Num > 50 && pm2_5Num <= 75) {
+                    pm2_5Color = Colors.orange;
+                    pm2_5Text = "나쁨";
+                  } else if (pm2_5Num > 75) {
+                    pm2_5Color = Colors.red;
+                    pm2_5Text = "최악";
+                  }
+                  if (pm10Num <= 20) {
+                    pm10Color = Colors.blue;
+                    pm10Text = "최상";
+                  } else if (pm10Num > 20 && pm10Num <= 50) {
+                    pm10Color = Colors.green;
+                    pm10Text = "좋음";
+                  } else if (pm10Num > 50 && pm10Num <= 100) {
+                    pm10Color = Colors.yellow;
+                    pm10Text = "보통";
+                  } else if (pm10Num > 100 && pm10Num <= 200) {
+                    pm10Color = Colors.orange;
+                    pm10Text = "나쁨";
+                  } else if (pm10Num > 200) {
+                    pm10Color = Colors.red;
+                    pm10Text = "최악";
+                  }
                   return Container(
                     padding: const EdgeInsets.symmetric(
                       horizontal: Sizes.size10,
@@ -118,12 +162,11 @@ class _TodayWeatherState extends ConsumerState<TodayWeather> {
                               width: 30,
                               height: 30,
                               decoration: BoxDecoration(
-                                color: Colors.red,
+                                color: pm2_5Color,
                                 border: Border.all(
-                                    width: 3,
-                                    color: Theme.of(context)
-                                        .primaryColor
-                                        .withOpacity(0.5)),
+                                  width: 3,
+                                  color: Colors.white,
+                                ),
                                 borderRadius: const BorderRadius.all(
                                   Radius.circular(
                                     100,
@@ -133,9 +176,7 @@ class _TodayWeatherState extends ConsumerState<TodayWeather> {
                             ),
                             Gaps.v10,
                             Text(
-                              snapshot.data!.airPollution
-                                  .map((airPollution) => airPollution.pm2_5)
-                                  .join(", "),
+                              "$pm2_5Text ($pm2_5Pollution)",
                               style: const TextStyle(fontSize: Sizes.size16),
                             ),
                           ],
@@ -151,12 +192,11 @@ class _TodayWeatherState extends ConsumerState<TodayWeather> {
                               width: 30,
                               height: 30,
                               decoration: BoxDecoration(
-                                color: Colors.red,
+                                color: pm10Color,
                                 border: Border.all(
-                                    width: 3,
-                                    color: Theme.of(context)
-                                        .primaryColor
-                                        .withOpacity(0.5)),
+                                  width: 3,
+                                  color: Colors.white,
+                                ),
                                 borderRadius: const BorderRadius.all(
                                   Radius.circular(
                                     100,
@@ -166,9 +206,7 @@ class _TodayWeatherState extends ConsumerState<TodayWeather> {
                             ),
                             Gaps.v10,
                             Text(
-                              snapshot.data!.airPollution
-                                  .map((airPollution) => airPollution.pm10)
-                                  .join(", "),
+                              "$pm10Text ($pm10Pollution)",
                               style: const TextStyle(fontSize: Sizes.size16),
                             ),
                           ],
